e61bcc455de6ed813c7c111daf3a8a271001a4dc
AisyahAmeliaZarahJuaita <131422249+AisyahAmeliaZarahJuaita@users.noreply.github.com>
2023-04-21 11:47:34 +0700

    Add files via upload

---
+++ kode 2.py
@@ -0,0 +1,9 @@
+abstract class Binatang {
+    abstract var apakahBisaBerenang: Boolean //abstract variabel
+
+    fun bernafas() {
+        println("Setiap binatang bisa bernafas!")
+    }
+
+    abstract fun caraBernafas() //abstract method
+}
