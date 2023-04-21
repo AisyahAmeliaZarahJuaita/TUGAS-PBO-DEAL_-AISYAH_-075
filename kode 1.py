3967e95c0626c67e3fd5549301895a1d2653337a
AisyahAmeliaZarahJuaita <131422249+AisyahAmeliaZarahJuaita@users.noreply.github.com>
2023-04-21 11:50:17 +0700

    Delete kode 1.py

--- kode 1.py
+++
@@ -1,7 +0,0 @@
-class mobil:
-    def __init__(self, jenis, warna):
-        print("ini mobil saya")
-        self.jenis = jenis
-        self.warna = warna
-mobil1 = mobil('Sedan', 'Merah')
-mobil2 = mobil('SUV', 'Hitam')
